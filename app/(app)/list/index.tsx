d22d5462dfa5f19adef96bb7c03329615fed40a1
Benjamin Dandre <dandre.ben@gmail.com>
2025-01-16 16:30:21 +0100

    install expo gradient

--- app/(app)/list/index.tsx
+++ app/(app)/list/index.tsx
@@ -1,21 +1,36 @@
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Colors } from '@/constants/Colors';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function ListIndex() {
 
     return (
-        <View style={{flex: 1, backgroundColor: Colors.pink100, justifyContent: 'center', alignItems: 'center'}}>
-            <View style={styles.paralaxHeader}>
+        <View style={{flex: 1, backgroundColor: Colors.pink100}}>
+            <LinearGradient
+                colors={[Colors.pink300, Colors.pink100]}
+                style={styles.gradient}
+            >
+                <View style={styles.paralaxHeader}>
 
-            </View>
-            <View style={styles.content}>
-                <Text>Index</Text>
-            </View>
+                </View>
+                <View style={styles.content}>
+                    <Text>Index</Text>
+                </View>
+            </LinearGradient>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    gradient: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        height: '100%',
+        justifyContent: 'center', 
+        alignItems: 'center'
+    },
     paralaxHeader: {
 
     },
--- app/(app)/parameters/index.tsx
+++ app/(app)/parameters/index.tsx
@@ -1,11 +1,29 @@
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Colors } from '@/constants/Colors';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function ParametersIndex() {
 
     return (
-        <View style={{flex: 1, backgroundColor: Colors.blue100, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Paramètres</Text>
+        <View style={{flex: 1, backgroundColor: Colors.blue100}}>
+            <LinearGradient
+                colors={[Colors.blue300, Colors.blue100]}
+                style={styles.gradient}
+            >
+                <Text>Parameters</Text>
+            </LinearGradient>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    gradient: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        height: '100%',
+        justifyContent: 'center', 
+        alignItems: 'center'
+    }
+})
