ac0faf3371ab54caa015f8fc709deaa5f5d7dc5c
Benjamin Dandre <dandre.ben@gmail.com>
2025-02-19 13:18:33 +0100

    Extra page layout improvement

--- app/(app)/extra/index.tsx
+++ app/(app)/extra/index.tsx
@@ -17,14 +17,16 @@ export default function ExtraIndex() {
 
     return (
         <View style={{flex: 1}}>
-            <PageContainer color1={Colors.teal300} color2={Colors.teal100} gradient>
-                <ThemedText type="title">Extra</ThemedText>
+            <PageContainer color1={Colors.teal500} color2={Colors.teal300} gradient>
+                <View style={{width: "100%"}}>
+                    <ThemedText type="title" style={{marginLeft: 16}}>Extra</ThemedText>
+                </View>
                 <View style={styles.extraCard}>
                     <ThemedText type="subtitle">5K Counter</ThemedText>
                     <ThemedText>
                         5K is a dice game where your goal is to reach 5000 points before your opponent. This app will provide a way to keep track of the score of all players.
                     </ThemedText>
-                    <CustomButton lightText hapticFeel color={{color1: Colors.blue500, color2: Colors.blue700}} text={"Go to 5000 counter"} onPress={() => router.push("/extra/five-thousand")}/>
+                    <CustomButton lightText hapticFeel color={{color1: Colors.blue500, color2: Colors.blue700}} text={"Start a Game"} onPress={() => router.push("/extra/five-thousand")}/>
                 </View>
             </PageContainer>
         </View>
