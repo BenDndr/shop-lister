8b552e32123a20e0d91fb2f25fca4f291d188fcc
Benjamin Dandre <dandre.ben@gmail.com>
2025-02-21 12:04:06 +0100

    5k interface + add player

--- app/(app)/extra/five-thousand.tsx
+++ app/(app)/extra/five-thousand.tsx
@@ -5,32 +5,53 @@ import { PageContainer } from "@/components/PageContainer"
 import { ThemedText } from "@/components/ThemedText"
 import { CustomInput } from "@/components/CustomInput"
 import { CustomButton } from "@/components/CustomButton"
-import { Link, useRouter } from "expo-router"
-import { useRoute } from '@react-navigation/native';
+import { useRouter } from "expo-router"
 import { useAppSelector, useAppDispatch } from '@/store/hooks'
+import { addPlayer } from '@/store/slices/fivekSlice'
 
 export default function FiveThousand() {
 
     const router = useRouter()
     const fivek = useAppSelector(state => state.fivek)
     const [newPlayer, setNewPlayer] = useState("")
+    const dispatch = useAppDispatch()
 
     console.log("fivek", fivek)
 
-    const addPlayer = () => {
-        console.log("newPlayer", newPlayer)
+    const createPlayer = () => {
+        dispatch(addPlayer(newPlayer))
         setNewPlayer("")
     }
 
     return (
-        <PageContainer color1={Colors.orange300} color2={Colors.orange100} gradient>
-            <ThemedText type="subtitle">5K</ThemedText>
-            <CustomInput placeholder="Enter your name" value={newPlayer} onChangeText={(e) => setNewPlayer(e)} validate={addPlayer}/>
-            <CustomButton hapticFeel color={{color1: Colors.teal300, color2: Colors.teal100}} text={"Go back"} onPress={() => router.push("/extra")}/>
+        <PageContainer color1={Colors.teal500} color2={Colors.teal300} gradient>
+            <View style={styles.content}>
+                <View style={styles.header}>
+                    <ThemedText type="title">5K</ThemedText>
+                </View>
+                <View style={styles.body}>
+                    <CustomInput placeholder="Enter your name" value={newPlayer} onChangeText={(e) => setNewPlayer(e)} validate={createPlayer}/>
+                    {fivek.players.map((player, index) => (
+                        <ThemedText key={index}>{player.name}</ThemedText>
+                    ))}
+                    <CustomButton hapticFeel lightText color={{color1: Colors.blue500, color2: Colors.blue700}} text={"Go back"} onPress={() => router.push("/extra")} style={{marginTop: "auto"}}/>
+
+                </View>
+            </View>
         </PageContainer>
     )
 }
 
-const StyleSheets = StyleSheet.create({
-
+const styles = StyleSheet.create({
+    content: {
+        width: "90%",
+        height: "100%",
+    },
+    header: {
+        height: "20%",
+        justifyContent: "flex-end"
+    },
+    body: {
+        height: "70%",
+    }
 })
