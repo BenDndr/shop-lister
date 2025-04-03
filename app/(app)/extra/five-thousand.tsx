3833ef4c3be076f3f53458443152b6e1a9a76805
Benjamin Dandre <dandre.ben@gmail.com>
2025-04-03 17:41:59 +0200

    prod version v1.0

--- app/(app)/extra/five-thousand.tsx
+++ app/(app)/extra/five-thousand.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react"
+import { useState } from "react"
 import {View, StyleSheet, FlatList, TouchableOpacity, KeyboardAvoidingView, Platform} from "react-native"
 import { Colors } from "@/constants/Colors"
 import { PageContainer } from "@/components/PageContainer"
